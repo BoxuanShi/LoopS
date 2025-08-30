f16e5a408e4bb8c07e7c303fabdb5acf7c925e37
Boxuan Shi <2356689164@qq.com>
2025-08-30 21:02:03 +0800

    add a comment.

--- LoopS.m
+++ LoopS.m
@@ -12,6 +12,7 @@ Get["LoadDependeces.m"];
 Print["################### LoopS: Dependences Loaded ##################"];
 
 
+(*LoopSInformation*)
 Print[Style["LoopS",FontFamily->"Arial",FontSize->14,FontColor->Black,Bold],
 Style[" - A Mathematica package for Feynman amplitudes reduction. By Bo-Xuan Shi (shibx@mail.nankai.edu.cn). 
 Version "<>$LoopSVersion<>".",FontFamily->"Arial",FontSize->14,FontColor->Black]
