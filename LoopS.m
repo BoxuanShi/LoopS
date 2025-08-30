8c75b199571de611a95dc89b569df4560bb35432
Boxuan Shi <2356689164@qq.com>
2025-08-30 20:57:47 +0800

    changed the file structures.

--- LoopS.m
+++ LoopS.m
@@ -2,7 +2,7 @@
 $LoopSVersion="2025-08-28";
 
 
-$LoopSInstallPath=DirectoryName[$InputFileName];
+$LoopSInstallPath = DirectoryName[$InputFileName];
 
 
 (*load-dependences*)
@@ -23,140 +23,32 @@ Style[" for more information.",FontFamily->"Arial",FontSize->14,FontColor->Black
 
 
 (*LoopS*)
-$Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"LoopS"}]];
-Get["LoopS.m"];
-Get["CreateProcess.m"];
-Get["ClearProcess.m"];
-Get["SetupProcess.m"];
-Get["SetProcessDirectory.m"];
-Get["FileStructures.m"];
-Get["PrepareParallel.m"];
-Get["PreparePV.m"];
-
-
-(*General*)
-$Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"General"}]];
-Get["DownValuesArguments.m"];
-Get["ParallelLoad.m"];
-Get["MMABugFix.m"];
-Get["Mul.m"];
-Get["FormDo.m"];
-Get["DistributeToPolyND.m"];
-Get["ListS.m"];
-Get["getS.m"];
-Get["TableS.m"];
-Get["getImS.m"];
-Get["sameSetQ.m"];
-Get["AtomizeRules.m"];
-Get["TableFormS.m"];
-Get["CoefficientS.m"];
-Get["CountS.m"];
-Get["texRational.m"];
-Get["SeriesS.m"];
-Get["SeriesPower.m"];
-Get["AbbreviateVariables.m"];
-Get["AbbreviateDeno.m"];
-Get["AbbreviatePolynomials.m"];
-Get["Separate.m"];
-Get["PolynomialCollect.m"];
-Get["CollectS.m"];
-Get["CollectFlat.m"];
-Get["ParallelTableS.m"];
-Get["groupByRationalRatios.m"];
-Get["generateSymmetryRules.m"];
-Get["ToStringHold.m"];
-Get["BlockCondition.m"];
-Get["CreateOptions.m"];
-Get["NotebookDirectoryS.m"];
-Get["OptRestrict.m"];
-Get["TogetherExpand.m"];
-Get["PathName.m"];
-Get["DropZeroByNumerics.m"];
-
-
+$Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"ProcessDefine"}]];
+Get["ProcessDefineLoad.m"];
+(*BasicS*)
+$Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"BasicS"}]];
+Get["BasicSLoad.m"];
 (*PVReduction*)
 $Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"PVReduction"}]];
-Get["GeneratePV.m"];
-Get["GeneratePVOPITeR.m"];
-Get["GeneratePVMMA.m"];
-Get["PVRules.m"];
-
-
+Get["PVReductionLoad.m"];
 (*TensorReduction*)
 $Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"TensorReduction"}]];
-Get["FCContract.m"];
-Get["FCES.m"];
-Get["ToSFADBugFix.m"];
-Get["PVReduction.m"];
-Get["SeparateFAD.m"];
-Get["ExpandDirac.m"];
-Get["IndexFunctions.m"];
-Get["OperatorCollect.m"];
-Get["AbbreviateOperators.m"];
-Get["NumeratorToSPD.m"];
-Get["PermutationRulesFromSets.m"];
-Get["CanonicalOperatorRules.m"];
-
-
+Get["TensorReductionLoad.m"];
 (*FeynmanIntegralsClassification*)
 $Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"FeynmanIntegralsClassification"}]];
-Get["zeroSectorQ.m"];
-Get["ApartFFS.m"];
-Get["DenoTransform.m"];
-Get["SPDToFAD.m"];
-Get["GetFeynInt.m"];
-Get["FamilyClassify.m"];
-Get["FindfamilyG.m"];
-Get["ReplaceSymmetryG.m"];
-Get["FindnumRules.m"];
-Get["ReduceSPDToG.m"];
-Get["DenominatorToG.m"];
-Get["AmplitudeReduce.m"];
-
-
+Get["FeynmanIntegralsClassificationLoad.m"];
 (*EquivalentIntegrals*)
 $Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"EquivalentIntegrals"}]];
-Get["CanonicalLoops.m"];
-Get["propsToLS.m"];
-Get["LSsubsets.m"];
-Get["SymanzikPolynomials.m"];
-Get["SymanzikOrder.m"];
-Get["SymanzikIndepentVars.m"];
-Get["sameFIQ.m"];
-Get["matchFI.m"];
-Get["MatchMIs.m"];
-Get["MFrelease.m"];
-Get["loopSymmetryNoPS.m"];
-
-
+Get["EquivalentIntegralsLoad.m"];
 (*FIREInterface*)
 $Path = Union@Append[$Path,FileNameJoin[{$LoopSInstallPath,"FIREInterface"}]];
-Get["FIRETemplate.m"];
-Get["FIREEvaluate.m"];
-Get["FIREPrepareStart.m"];
-Get["FIREPrepareStartMMA.m"];
-Get["FIRELoadStart.m"];
-Get["FIRELoadTable.m"];
-Get["FIREGetGRules.m"];
-Get["FIREReductionMMA.m"];
-Get["FIREPrepareCXX.m"];
-Get["FIRERunCXX.m"];
-Get["FIREReductionCXX.m"];
-Get["tosectors.m"];
-Get["PossibleIntForDEInSector.m"];
-Get["FindCompleteGList.m"];
-Get["GenerateEiknolFamilies.m"];
-Get["FindRulesComplete.m"];
-
-
+Get["FIREInterfaceLoad.m"];
 (*AMFlowInterface*)
 $Path = Union@Append[$Path, FileNameJoin[{$LoopSInstallPath,"AMFlowInterface"}]];
-Get["AMFlowInterface.m"];
-
-
+Get["AMFlowInterfaceLoad.m"];
 (*MasterIntegralResults*)
 $Path = Union@Append[$Path, FileNameJoin[{$LoopSInstallPath,"MasterIntegralResults"}]];
-Get["OneLoopExamples.m"];
+Get["MasterIntegralResultsLoad.m"];
 
 
 (*self-consistence check*)
