5c6258bd6954b2d90e9f1d7d829df2f7767be605
Boxuan Shi <2356689164@qq.com>
2025-09-03 04:47:35 +0800

    modified DiracPattern

--- ProcessDefine/LoopSDefine.m
+++ ProcessDefine/LoopSDefine.m
@@ -18,7 +18,7 @@ LoopS = <|
 
 SimplifyS = If[ToString[SimplifyS] === "SimplifyS", Factor, SimplifyS];
 
-DiracPattern = _Dot | _Spinor | _GAD | _GSD | _DiracTrace;
+DiracPattern = _DiracTrace| _Dot | _Spinor | _GAD | _GSD;
 OperatorPattern = 
   If[ToString@OperatorPattern === "OperatorPattern", DiracPattern, 
    OperatorPattern];
