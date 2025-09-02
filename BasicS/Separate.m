c1b9be420c137dd1d274910a69b6d5cafe37f167
Boxuan Shi <2356689164@qq.com>
2025-09-02 17:03:55 +0800

    Separate is now listable for the first argument. add option SeparateHead.

--- BasicS/Separate.m
+++ BasicS/Separate.m
@@ -1,12 +1,30 @@
 Separate;
 
-ClearAll[Separate, SeparatePoly];
-Separate[expr_, patt_] := Module[{x, vars},
-  vars = getS[{expr}, patt];
-  SeparatePoly[expr, vars]
-  ]
+ClearAll[Separate, Separate0, Separate1, SeparatePoly];
+SeparateHead;
+Separate::usage = 
+  "1. Separate[expr, patt] separate polynomials consitituted by patt.
+2. Option SeparateHead replace the default head List.
+3. Separate has attribute Listable for the first argument.";
 
-Separate[expr_, patt0_List] := Module[{i, coe, patt, tp1, tp2, dvQ, matchQ},
+Options[Separate] := CreateOptions[{}, {Separate1}];
+Separate[expr_, patt_, opt : OptionsPattern[]] /; OptRestrict[opt] := 
+ Separate0[expr, "patt" -> patt, 
+  Evaluate@FilterOptions[{opt}, Separate1]]
+Options[Separate0] := CreateOptions[{"patt" -> "patt"}, {Separate1}]
+SetAttributes[Separate0, Listable];
+Separate0[expr_, opt : OptionsPattern[]] /; OptRestrict[opt] := 
+ Separate1[expr, OptionValue["patt"], 
+  Evaluate@FilterOptions[{opt}, Separate1]]
+
+Options[Separate1] := CreateOptions[{}, {SeparatePoly}];
+Separate1[expr_, patt_, opt : OptionsPattern[]] /; OptRestrict[opt] :=
+  Module[{x, vars}, vars = getS[{expr}, patt];
+  SeparatePoly[expr, vars, 
+   Evaluate@FilterOptions[{opt}, SeparatePoly]]]
+
+Separate1[expr_, patt0_List, opt : OptionsPattern[]] /; 
+  OptRestrict[opt] := Module[{i, coe, patt, tp1, tp2, dvQ, matchQ},
   {coe, patt} = Separate[expr, Alternatives @@ patt0];
   patt = (ListS[#, {}, Times] &) /@ patt;
   patt = (Table[
@@ -14,17 +32,18 @@ Separate[expr_, patt0_List] := Module[{i, coe, patt, tp1, tp2, dvQ, matchQ},
         Length@patt0}] &) /@ patt;
   patt = (Times @@@ # &) /@ patt;
   patt = Transpose@patt;
-  {coe, Sequence @@ patt}
-  ]
+  OptionValue["SeparateHead"] @@ {coe, Sequence @@ patt}]
 
-SeparatePoly[expr_, vars_List] := Module[{sa, ar, ltop, tp1},
-  If[expr === 0, Return[{{}, {}}]];
-  If[vars === {}, Return[{{expr}, {1}}]];
+Options[SeparatePoly] = {"SeparateHead" -> List};
+SeparatePoly[expr_, vars_List, opt : OptionsPattern[]] /; 
+  OptRestrict[opt] := Module[{sa, ar, ltop, tp1},
+  If[expr === 0, Return[OptionValue["SeparateHead"] @@ {{}, {}}]];
+  If[vars === {}, 
+   Return[OptionValue["SeparateHead"] @@ {{expr}, {1}}]];
   sa = CoefficientArrays[expr, vars];
   ar = (ArrayRules /@ sa[[2 ;; -1]])[[All, 1 ;; -2]] // Flatten;
   ltop[list_] := Times @@ (vars[[#]] & /@ list);
   tp1 = ar /. (x_ -> y_) :> {y, ltop[x]};
   tp1 = SortBy[tp1, Last];
   If[sa[[1]] === 0, Nothing, PrependTo[tp1, {sa[[1]], 1}]];
-  Transpose[tp1]
-  ]
+  OptionValue["SeparateHead"] @@ Transpose[tp1]]
