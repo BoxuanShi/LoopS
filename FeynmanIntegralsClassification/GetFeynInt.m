7ce75757b675f81868c96e60054b91536dad24f3
Balth <2356689164@qq.com>
2025-09-09 12:36:06 +0800

    fix a bug for FCI amplitudes input for GetFeynInt.

--- FeynmanIntegralsClassification/GetFeynInt.m
+++ FeynmanIntegralsClassification/GetFeynInt.m
@@ -18,15 +18,15 @@ GetFeynInt[amp_List, loops_List, moms_List, kinematics_List,
      Evaluate@opttable] // Flatten // Union]
 
 GetFeynInt[amp_, loops_List, moms_List, kinematics_List] := 
- Module[{patt, i, num, tpm1, tp0, tp1, optAP},
+ Module[{i, patt, tp0, tp1},
   If[
-   MatchQ[amp, _FAD | _SFAD] && 
+   MatchQ[amp, _FAD | _SFAD | _FeynAmpDenominator] && 
     AllTrue[amp, ! FreeQ[#, Alternatives @@ loops] &], 
    Return[ApartFFS[amp, loops, moms, kinematics, 
        "ApartFFSSimplify" -> (# &)][[2]] // getS[#, _FAD | _SFAD] &]
    ];
   
-  patt = x_FAD | x_SFAD /; 
+  patt = x_FAD | x_SFAD | x_FeynAmpDenominator /; 
     Or @@ Table[! FreeQ[x, loops[[i]]], {i, Length@loops}];
   
   tp0 = amp // PolynomialCollect[#, patt] & // FeynAmpDenominatorSplit // FCES // PolynomialCollect[#, patt] &;
