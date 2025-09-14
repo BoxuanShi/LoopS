325bd823775d96332629fb3d575303a8ab0baaf6
Balth <2356689164@qq.com>
2025-09-15 01:20:29 +0800

    same

--- BasicS/GenerateSymmetryRules.m
+++ BasicS/GenerateSymmetryRules.m
@@ -4,7 +4,7 @@ variables of "1, 2, 3, 4..."*)
 
 ClearAll[GenerateSymmetryRules]
 GenerateSymmetryRules[generators_, symbollist_] := 
-  Module[{i, j, num, tp1, tp2, tp3, tp4},
+  Module[{tp1, tp2, tp3, tp4},
    tp1 = PermutationProduct /@ 
      GroupElements[PermutationGroup[Cycles /@ generators]];
    tp2 = InversePermutation /@ tp1;
@@ -21,7 +21,7 @@ GenerateSymmetryRules[generators_, symbollist_] :=
          Length@Dimensions@symbollist] // Flatten // Union
      , {i, Length@tp2}];
      
-   <|"Rules" -> tp3, "InvRules" -> tp4|>
+   <|"Rules" -> (AtomizeRules /@ tp3), "InvRules" -> (AtomizeRules /@ tp4)|>
    
    ];
 
