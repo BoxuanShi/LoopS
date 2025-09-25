70f57bb2eb04877ba536484d873bf658dcee642c
Balth <2356689164@qq.com>
2025-09-25 20:53:51 +0800

    Update SeparateFAD: loops -> loopmoms.

--- TensorReduction/SeparateFAD.m
+++ TensorReduction/SeparateFAD.m
@@ -1,16 +1,19 @@
 ClearAll[SeparateFAD]
-SeparateFAD[amp_, loops_, process_String : "CurrentProcess"] := 
- SeparateFAD[amp, loops, ToExpression[process]]
-SeparateFAD[amp_, loops_, process_Association] := 
- SeparateFAD[amp, loops, process["moms"]]
-SeparateFAD[amp_, loops_List, moms_List] := 
- Module[{amp2, tp1, tp2, patt, i, tpNumlist, tpDenolist, tpNumSPD},
-  patt = x : (_FAD | _SFAD) /; ! FreeQ[x, Alternatives @@ loops];
+SeparateFAD[amp_, process_String : "CurrentProcess"] := 
+ SeparateFAD[amp, ToExpression[process]]
+SeparateFAD[amp_, process_Association] := 
+ SeparateFAD[amp, process["loopmoms"], process["moms"]]
+SeparateFAD[amp_, loopmoms_List, moms_List] := 
+ Module[{amp2, patt, i, tpNumlist, tpDenolist},
+
+  patt = x : (_FAD | _SFAD) /; ! FreeQ[x, Alternatives @@ loopmoms];
+
   amp2 = If[! FreeQ[amp, SFAD], amp // ToSFAD, 
-    amp];(*fix a bug of FeynAmpDenominatorCombine*)
+    amp];
+
   {tpNumlist, tpDenolist} = 
    amp2 // FeynAmpDenominatorSplit // FCES // Separate[#, patt] & // 
      FeynAmpDenominatorCombine // FCES;
-  (*tpNumlist=tpNumlist//FeynAmpDenominatorExplicit//ExpandDirac[#,moms]&;*)
+
   {tpNumlist, tpDenolist}
   ]
