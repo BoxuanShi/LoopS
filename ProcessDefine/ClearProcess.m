c5eca66c180e74fd7f5c014d4a72a478b87c2fd1
Balth <2356689164@qq.com>
2025-09-28 17:38:20 +0800

    Update ClearProcess. ClearProcess can automatically detect Set and SetDelayed in AlgebrasDefinition now.

--- ProcessDefine/ClearProcess.m
+++ ProcessDefine/ClearProcess.m
@@ -1,9 +1,17 @@
-ClearProcess[] := Module[{},
-  (*Clear defined FeynCalcs - general*)
+ClearProcess[] := Module[{tp1, tp2, tp3, a, b, x1},
+
+  tp1 = DownValues[AlgebrasDefinition];
+  tp2 = tp1 /. CompoundExpression -> List;
+  tp3 = Reap[
+    Scan[Function[x1, If[MatchQ[Head[Unevaluated[x1]], Set|SetDelayed], Sow[Hold[x1]]], HoldAll], tp2, {0, Infinity}]
+  ][[2]]//Flatten;
+  tp3 /. HoldPattern[(a_:=b_)|(a_=b_)] :> (a =.) // ReleaseHold;
+
+  (*Clear user defined FeynCalcs - general*)
   FCClearScalarProducts[];
-  UpValues[Spinor] =.;
   DataType[__, _] := False;
-  (*Clear defined FeynCalcs - special*)
+
+  (*Clear user defined - special*)
   ClearProcess /@ DefinedProcess;
   
   (*Distribute to CurrentProcess*)
