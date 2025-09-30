9d700fe2f486d5f557888613f1cd687b023b880d
Balth <2356689164@qq.com>
2025-09-30 16:02:53 +0800

    getdummyindices: Fix a FCGetDummyIndices bug.

--- TensorReduction/IndexFunctions.m
+++ TensorReduction/IndexFunctions.m
@@ -5,7 +5,7 @@ getfullindices[amp_, patt_ : _GAD | _GSD | _GA | _GS | _FV | _FVD] :=
 (*DotSimplify is to avoid bugs in some computer in the case of self-defined EOM \
 exist.*)
 getdummyindices[amp_, head_ : LorentzIndex] := 
- FCGetDummyIndices[DotSimplify @ FCI @ amp, Flatten@{head}]
+ FCGetDummyIndices[ListS @ DotSimplify @ FCI @ amp, Flatten@{head}]
 
 (*get dummy index for every spinor..., input should be one operator*)
 getdummyindicesList[expr_] := Module[{tp1, tp2, tp3},
