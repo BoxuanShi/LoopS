f7e34ca60d37107e88ba1410a42f85ee2fd79c43
Balth <2356689164@qq.com>
2025-11-12 16:56:06 +0800

    CollectS: bug fix/

--- BasicS/CollectS.m
+++ BasicS/CollectS.m
@@ -1,7 +1,7 @@
 ClearAll[CollectS]
 CollectS[expr_List, patt_, func1_ : (# &), func2_ : (# &)] := CollectS[#, patt, func1, func2] & /@ expr
 CollectS[expr_, patt_, func1_ : (# &), func2_ : (# &)] := Module[{a, b},
-  {a, b} = Separate[expr, patt];
+  {a, b} = Separate[expr, Alternatives@@Flatten[{patt}]];
   a = func1 /@ a;
   b = func2 /@ b;
   a . b]
